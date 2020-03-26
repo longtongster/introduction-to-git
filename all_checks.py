838b5c241fec10fe27fca71086f059c671154f8f
Longtong <sacha.van.weeren@gmail.com>
2020-03-26 17:03:19 +0100

    Added sys.exit

--- all_checks.py
+++ all_checks.py
@@ -2,13 +2,17 @@
 
 # New file
 
+import sys
+
 def check_reboot():
     return False
 
 def main():
     if check_reboot():
         print("Pending Reboot.")
+        sys.exit(1)
     else:
         print("All ok")
+        sys.exit(0)
 
 main()
