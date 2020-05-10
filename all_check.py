5d317261aa35bf4416828489d89c85b8a006158b
longtongster <sacha.van.weeren@gmail.com>
2020-05-10 08:02:32 +0200

    small change added

--- all_check.py
+++ all_check.py
@@ -6,7 +6,8 @@ import sys
 import os
 
 def check_reboot():
-    return False
+    """ Returns True if the computer has a pending reboot."""
+    return os.path.exists("/run/reboot-required")
 
 def main():
     if check_reboot():
